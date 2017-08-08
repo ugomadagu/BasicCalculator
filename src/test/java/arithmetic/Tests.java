1042a0f25f87017786016106651f7054e64ad47e
Ugo Madagu <ugomadagu@Ugos-MacBook-Pro.local>
2017-08-08 10:35:11 -0400

    Added tests

---
+++ src/test/java/arithmetic/Tests.java
@@ -0,0 +1,125 @@
+package arithmetic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import arithmetic.*;
+
+public class Tests{
+  Calculator calc = new Calculator();
+
+  @Test
+  public void BasicAddition() {
+    String expression = "2 + 4";
+    double expectedAnswer = 6.0;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void BasicSubtraction() {
+    String expression = "2 - 4";
+    double expectedAnswer = -2.0;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void BasicMultiplication() {
+    String expression = "2 * 4";
+    double expectedAnswer = 8.0;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void BasicDivision() {
+    String expression = "2 / 4";
+    double expectedAnswer = 0.5;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void OrderOfOperationsTest() {
+    String expression = "2 + 4 - 6 * 3 + 8";
+    double expectedAnswer = -4.0;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void DoubleNegativeTest() {
+    String expression = "2 - -4 - 6 * 3 + 8";
+    double expectedAnswer = -4.0;
+
+    try {
+      assertTrue(calc.evaluate(expression) == expectedAnswer);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  @Test
+  public void BadOperatorTest() {
+    String expression = "2 # 3";
+    boolean exceptionThrown = false;
+
+    try {
+      calc.evaluate(expression);
+    } catch (Exception e) {
+      exceptionThrown = true;
+      assertTrue(e.toString().equals("java.lang.IllegalArgumentException: Bad Operator."));
+    }
+    assertTrue(exceptionThrown);
+  }
+
+  @Test
+  public void EmptyExpressionTest() {
+    String expression = "";
+    boolean exceptionThrown = false;
+
+    try {
+      calc.evaluate(expression);
+    } catch (Exception e) {
+      exceptionThrown = true;
+      assertTrue(e.toString().equals("java.lang.IllegalArgumentException: Expression must have at least two operands and one operator."));
+    }
+    assertTrue(exceptionThrown);
+  }
+
+  @Test
+  public void BadExpressionTest() {
+    String expression = "2 + 3 -";
+    boolean exceptionThrown = false;
+
+    try {
+      calc.evaluate(expression);
+    } catch (Exception e) {
+      exceptionThrown = true;
+      assertTrue(e.toString().equals("java.lang.IllegalArgumentException: Bad expression."));
+    }
+    assertTrue(exceptionThrown);
+  }
+
+
+}
