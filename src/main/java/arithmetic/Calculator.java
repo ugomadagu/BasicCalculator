23ba7f4473c0f2b46d0f8ab94fb623c0bfb03e8b
Ugo Madagu <ugomadagu@Ugos-MacBook-Pro.local>
2017-08-08 00:12:50 -0400

    Changed return type for 'evaluate' function and other code changes revolving around that

--- src/main/java/arithmetic/Calculator.java
+++ src/main/java/arithmetic/Calculator.java
@@ -4,38 +4,34 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-  public String evaluate(String expression) {
+  public double evaluate(String expression) throws Exception {
     if(expression == null || expression.equals("") || expression.matches("\\s+")) {
       throw new IllegalArgumentException("Expression must have at least two operands and one operator.");
     }
 
     if(expression.matches("(\\-*\\d+(\\.\\d+)*)")) {
-      return expression;
+      return Double.parseDouble(expression);
     }
 
     String pattern = "([\\w\\.\\-*]+)\\s*([^\\w\\.\\s])\\s*([\\w\\.\\-*]+)\\s*(.*)";
     Pattern r = Pattern.compile(pattern);
     Matcher m = r.matcher(expression);
 
-    String result;
+    double result;
 
     if (m.find()) {
-      String operand1 = m.group(1);
+      double operand1 = Double.parseDouble(m.group(1));
       String operator = m.group(2);
-      String operand2 = m.group(3);
+      double operand2 = Double.parseDouble(m.group(3));
       String restOfExpression = m.group(4);
 
       if(operator.equals("+")) {
-        result = doMath(operand1, operator, evaluate(operand2 + restOfExpression));
+        result = doMath(operand1, "+", evaluate(operand2 + restOfExpression));
       } else if(operator.equals("-")) {
-        if(operand2.charAt(0) == '-') {
-          operand2 = operand2.substring(1);
-        } else {
-          operand2 = "-" + operand2;
-        }
+        operand2 = operand2 * -1.0;
         result = doMath(operand1, "+", evaluate(operand2 + restOfExpression));
       } else {
-        String firstCalculation = doMath(operand1, operator, operand2);
+        double firstCalculation = doMath(operand1, operator, operand2);
         result = evaluate(firstCalculation + restOfExpression);
       }
     } else {
@@ -45,21 +41,19 @@ public class Calculator {
     return result;
   }
 
-  private String doMath(String operand1, String operator, String operand2) {
+  private double doMath(double operand1, String operator, double operand2) {
     double result = -1.0;
-    double num1 = Double.parseDouble(operand1);
-    double num2 = Double.parseDouble(operand2);
     if(operator.equals("*")) {
-      result = num1 * num2;
+      result = operand1 * operand2;
     } else if(operator.equals("/")) {
-      result = num1 / num2;
+      result = operand1 / operand2;
     } else if(operator.equals("+")) {
-      result = num1 + num2;
+      result = operand1 + operand2;
     } else if(operator.equals("-")) {
-      result = num1 - num2;
+      result = operand1 - operand2;
     } else {
       throw new IllegalArgumentException("Bad Operator.");
     }
-    return Double.toString(result);
+    return result;
   }
 }
